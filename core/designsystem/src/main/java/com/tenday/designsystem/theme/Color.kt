d1820f69f970495ba6c2945b39f76fe63c967ff1
SANDY <nnal0256@naver.com>
2025-01-08 16:48:55 +0900

    add: 색상 시스템 추가

---
+++ core/designsystem/src/main/java/com/tenday/designsystem/theme/Color.kt
@@ -0,0 +1,34 @@
+package com.tenday.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val HandsUpBlue = Color(0xFF3D75EE)
+val HandsUpBlueSub = Color(0xFFE8F0FF)
+
+val HandsUpGreen = Color(0xFF399500)
+val HandsUpGreenSub = Color(0xFFEFF7EB)
+
+val HandsUpPurple = Color(0xFF7200B5)
+val HandsUpPurpleSub = Color(0xFFF4EBF9)
+
+val HandsUpOrange = Color(0xFFFA610F)
+val HandsUpOrangeSub = Color(0xFFFFF2EC)
+
+val HandsUpBlack = Color(0xFF2A3038)
+val HandsUpBlackSub = Color(0xFFF7F8FA)
+
+val Gray900 = Color(0xFF111111)
+val Gray800 = Color(0xFF333333)
+val Gray700 = Color(0xFF505050)
+val Gray600 = Color(0xFF767676)
+val Gray500 = Color(0xFF999999)
+val Gray400 = Color(0xFFBBBBBB)
+val Gray300 = Color(0xFFCCCCCC)
+val Gray200 = Color(0xFFE5E5EC)
+val Gray100 = Color(0xFFF1F1F5)
+val Gray50 = Color(0xFFF7F7FB)
+
+val White = Color(0xFFFFFFFF)
+val Positive = Color(0xFF41C321)
+val Negative = Color(0xFFFF4D4D)
