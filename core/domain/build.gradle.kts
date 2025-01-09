f4e7d53d4c4465c5daaaa78002ef7a57a62129ee
SANDY <nnal0256@naver.com>
2025-01-09 12:54:27 +0900

    add: domain모듈 세팅

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,4 +1,28 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.tenday.core.domain"
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 29
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:data"))
+
+    implementation(libs.javax.inject)
+    implementation(libs.kotlinx.coroutines.android)
 }
