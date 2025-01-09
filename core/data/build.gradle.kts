664cbc0da775160b68d0d704954a62944a56f78f
SANDY <nnal0256@naver.com>
2025-01-09 13:08:46 +0900

    add: data 모듈 세팅

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,4 +1,29 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+}
+android {
+    namespace = "com.tenday.core.data"
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 29
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":core:network"))
+    implementation(project(":core:domain"))
+
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
 }
