77669f63631404e162fd4fd6755176efd7393cb0
SANDY <nnal0256@naver.com>
2025-01-10 18:16:04 +0900

    feat: 프로필카드 프레임 UI 만들기

---
+++ core/designsystem/src/main/java/com/tenday/designsystem/components/profileCard/ProfileCard.kt
@@ -0,0 +1,108 @@
+package com.tenday.designsystem.components.profileCard
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tenday.designsystem.R
+import com.tenday.designsystem.dimens.Dimens
+import com.tenday.designsystem.extentions.dashedFill
+import com.tenday.designsystem.icons.HandsUpLogo
+import com.tenday.designsystem.theme.Gray300
+
+@Composable
+fun ProfileCard(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.height(390.dp).width(320.dp)
+    ) {
+        ProfileFrameField()
+        Column(
+            modifier = modifier.padding(
+                top = Dimens.margin32,
+                start = Dimens.margin32,
+                end = Dimens.margin32,
+                bottom = Dimens.margin32,
+            )
+        ) {
+            ProfileCardHeader()
+            Spacer(modifier = modifier.height(Dimens.margin6))
+            Image(
+                modifier = modifier.size(100.dp)
+                    .align(Alignment.CenterHorizontally),
+                painter = painterResource(R.drawable.test),
+                contentDescription = null,
+            )
+            Spacer(modifier = modifier.height(Dimens.margin10))
+            ProfileCardName(modifier = modifier.align(Alignment.CenterHorizontally))
+            Spacer(modifier = modifier.height(Dimens.margin6))
+            ProfileCardInfo(modifier = modifier.align(Alignment.CenterHorizontally))
+            Spacer(modifier = modifier.height(Dimens.margin16))
+            Box(
+                modifier = modifier.fillMaxWidth()
+                    .height(2.dp)
+                    .padding(horizontal = Dimens.margin12)
+                    .dashedFill(
+                        color = Gray300,
+                    )
+            )
+            Spacer(modifier = modifier.height(Dimens.margin16))
+            ProfileCardLevel()
+            Spacer(modifier = modifier.height(Dimens.margin16))
+            Icon(
+                modifier = modifier.align(Alignment.CenterHorizontally).alpha(0.2f),
+                imageVector = Icons.HandsUpLogo,
+                contentDescription = null,
+            )
+        }
+    }
+}
+
+@Composable
+private fun ProfileFrameField(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+    ) {
+        Image(
+            painter = painterResource(R.drawable.profile_frame_field_01),
+            contentDescription = null,
+        )
+        Image(
+            modifier = modifier.weight(1f),
+            painter = painterResource(R.drawable.profile_frame_field_02),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+        )
+        Image(
+            painter = painterResource(R.drawable.profile_frame_field_03),
+            contentDescription = null,
+        )
+    }
+}
+
+
+@Preview(name = "ProfileCard")
+@Composable
+private fun PreviewProfileCard() {
+    ProfileCard()
+    //ProfileFrameField(modifier = Modifier.size(320.dp, 380.dp))
+}
---
+++ core/designsystem/src/main/java/com/tenday/designsystem/components/profileCard/ProfileCardHeader.kt
@@ -0,0 +1,78 @@
+package com.tenday.designsystem.components.profileCard
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tenday.designsystem.dimens.Dimens
+import com.tenday.designsystem.icons.HandsUp
+import com.tenday.designsystem.icons.Settings
+import com.tenday.designsystem.theme.Gray400
+import com.tenday.designsystem.theme.Gray600
+import com.tenday.designsystem.theme.HandsUpTypography
+
+@Composable
+internal fun ProfileCardHeader(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            modifier = modifier
+                .clip(CircleShape)
+                .size(20.dp),
+            imageVector = Icons.HandsUp,
+            contentDescription = null,
+        )
+        Spacer(modifier = modifier.width(Dimens.margin4))
+        Text(
+            text = "입사일",
+            style = HandsUpTypography.body4.copy(
+                fontWeight = FontWeight.SemiBold,
+            ),
+            color = Gray600,
+        )
+        Text(
+            text = "·",
+            style = HandsUpTypography.body4.copy(
+                fontWeight = FontWeight.SemiBold,
+            ),
+            color = Gray600,
+        )
+        Text(
+            text = "2024.12.20",
+            style = HandsUpTypography.body4.copy(
+                fontWeight = FontWeight.SemiBold,
+            ),
+            color = Gray600,
+        )
+        Spacer(modifier = modifier.weight(1f))
+        Icon(
+            modifier = modifier.size(24.dp),
+            imageVector = Icons.Settings,
+            contentDescription = null,
+            tint = Gray400,
+        )
+    }
+}
+
+@Preview(name = "ProfileCardHeader")
+@Composable
+private fun PreviewProfileCardHeader() {
+    ProfileCardHeader()
+}
