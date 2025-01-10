8ea1da59b28a0ae58f14a76fcb9e9a0a35e8b1a4
SANDY <nnal0256@naver.com>
2025-01-10 21:13:28 +0900

    add: HomeScreen 리소스 추가

--- feature/home/src/main/java/com/tenday/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/tenday/feature/home/HomeScreen.kt
@@ -1,17 +1,24 @@
 package com.tenday.feature.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun HomeScreen(
+internal fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
         Text(text = "HomeScreen")
+        Image(
+            painter = painterResource(R.drawable.profile_background_field),
+            contentDescription = null,
+        )
     }
 }
 
