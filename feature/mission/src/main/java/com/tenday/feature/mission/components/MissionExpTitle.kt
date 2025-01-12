88b0c0eb5a777299aab43e674dcde755ddbc3fd5
SANDY <nnal0256@naver.com>
2025-01-12 14:03:23 +0900

    feat: 리더부여-경험치 UI 구현

---
+++ feature/mission/src/main/java/com/tenday/feature/mission/components/MissionExpTitle.kt
@@ -0,0 +1,72 @@
+package com.tenday.feature.mission.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tenday.designsystem.dimens.Dimens
+import com.tenday.designsystem.icons.Dodoong
+import com.tenday.designsystem.theme.Gray100
+import com.tenday.designsystem.theme.HandsUpOrange
+import com.tenday.designsystem.theme.HandsUpTypography
+import com.tenday.feature.mission.R
+
+@Composable
+internal fun MissionExpTitle(
+    type: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = Gray100,
+            )
+            .padding(
+                top = Dimens.margin20,
+                bottom = Dimens.margin16,
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = "$type"+ stringResource(R.string.mission_exp_title),
+            style = HandsUpTypography.title5.copy(
+                fontWeight = FontWeight.ExtraBold,
+            )
+        )
+        Spacer(modifier = modifier.width(Dimens.margin8))
+        Text(
+            text = "2440",
+            style = HandsUpTypography.title5.copy(
+                fontWeight = FontWeight.ExtraBold,
+                color = HandsUpOrange,
+            )
+        )
+        Spacer(modifier = modifier.width(Dimens.margin2))
+        Image(
+            modifier = modifier.size(18.dp),
+            imageVector = Icons.Dodoong,
+            contentDescription = null,
+        )
+    }
+}
+
+@Preview(name = "MissionExpTitle")
+@Composable
+private fun PreviewMissionExpTitle() {
+    MissionExpTitle("리더부여")
+}
--- feature/mission/src/main/java/com/tenday/feature/mission/components/leader/MissionLeaderView.kt
+++ feature/mission/src/main/java/com/tenday/feature/mission/components/leader/MissionLeaderView.kt
@@ -1,22 +1,98 @@
 package com.tenday.feature.mission.components.leader
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tenday.designsystem.components.missionCard.MonthlyMissionCard
+import com.tenday.designsystem.components.missionCard.WeeklyMissionCard
+import com.tenday.designsystem.dimens.Dimens
+import com.tenday.designsystem.theme.White
+import com.tenday.feature.mission.R
+import com.tenday.feature.mission.components.MissionExpTitle
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun MissionLeaderView(
+    jobFamily: String,
+    jobGroup: Int,
+    onShowTooltip: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-
+    val pagerState = rememberPagerState (
+        pageCount = { 2 }
+    )
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        HorizontalPager(
+            modifier = modifier.fillMaxWidth().background(color = White),
+            state = pagerState,
+            pageSize = PageSize.Fixed(300.dp),
+            pageSpacing = Dimens.margin8,
+            contentPadding = PaddingValues(
+                start = Dimens.margin20,
+                end = Dimens.margin20,
+                bottom = Dimens.margin16
+            )
+        ) { page ->
+            when(page) {
+                0 -> ImproveWorkMissionCard(
+                    jobFamily = jobFamily,
+                    jobGroup = jobGroup,
+                    onShowTooltip = onShowTooltip
+                )
+                else -> SpecialWorkMissionCard(
+                    jobFamily = jobFamily,
+                    jobGroup = jobGroup,
+                    onShowTooltip = onShowTooltip
+                )
+            }
+        }
+        LazyColumn(
+            modifier = modifier.fillMaxSize(),
+            contentPadding = PaddingValues(
+                start = Dimens.margin20,
+                end = Dimens.margin20,
+                bottom = Dimens.margin24,
+                top = 0.dp
+            ),
+        ) {
+            stickyHeader {
+                MissionExpTitle(
+                    type = stringResource(R.string.mission_leader_title)
+                )
+            }
+            items(5) {
+                WeeklyMissionCard()
+                Spacer(modifier = modifier.height(Dimens.margin12))
+            }
+            item {
+                MonthlyMissionCard()
+            }
+        }
     }
 }
 
 @Preview(name = "MissionLeaderView")
 @Composable
 private fun PreviewMissionLeaderView() {
-    MissionLeaderView()
+    Box {
+        MissionLeaderView("음성 1센터", 1, {})
+    }
 }
