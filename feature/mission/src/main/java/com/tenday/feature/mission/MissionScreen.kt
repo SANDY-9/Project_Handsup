33d098cfd7dfb2a44e604337116452e97e20579e
SANDY <nnal0256@naver.com>
2025-01-12 16:49:41 +0900

    feat: 미션화면 탭메뉴 연결

--- feature/mission/src/main/java/com/tenday/feature/mission/MissionScreen.kt
+++ feature/mission/src/main/java/com/tenday/feature/mission/MissionScreen.kt
@@ -1,29 +1,56 @@
 
 package com.tenday.feature.mission
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tenday.feature.mission.navigation.MissionRoute
+import com.tenday.designsystem.theme.Gray100
+import com.tenday.feature.mission.components.MissionTitleBar
+import com.tenday.feature.mission.components.job.MissionJobView
+import com.tenday.feature.mission.components.leader.MissionLeaderView
+import com.tenday.feature.mission.components.personnel.MissionPersonnelView
+import com.tenday.feature.mission.components.project.MissionProjectView
 
 @Composable
 internal fun MissionRoute() {
-    MissionScreen()
+    var currentClickTab by remember { mutableStateOf(MissionMenu.리더부여) }
+    MissionScreen(currentClickTab, { currentClickTab = it })
 }
 
 @Composable
 internal fun MissionScreen(
-    modifier: Modifier = Modifier
+    currentTab: MissionMenu,
+    onTabSelect: (MissionMenu) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
-    Box(modifier) {
-        Text(text = "MissionScreen")
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Gray100),
+    ) {
+        MissionTitleBar(
+            currentSelectedTab = currentTab,
+            onTabSelect = onTabSelect,
+        )
+        when(currentTab) {
+            MissionMenu.리더부여 -> MissionLeaderView("음성 1센터", 1, {})
+            MissionMenu.직무미션 -> MissionJobView("음성 1센터", 1, {})
+            MissionMenu.전사프로젝트 -> MissionProjectView()
+            MissionMenu.인사평가 -> MissionPersonnelView()
+        }
     }
 }
 
 @Preview(name = "MissionScreen")
 @Composable
 private fun PreviewMissionScreen() {
-    MissionScreen()
+    var currentClickTab by remember { mutableStateOf(MissionMenu.리더부여) }
+    MissionScreen(currentClickTab, { currentClickTab = it })
 }
--- feature/mission/src/main/java/com/tenday/feature/mission/components/MissionTitleBar.kt
+++ feature/mission/src/main/java/com/tenday/feature/mission/components/MissionTitleBar.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -39,6 +39,7 @@ internal fun MissionTitleBar(
             currentSelectedTab = currentSelectedTab,
             onTabSelect = onTabSelect,
         )
+        Spacer(modifier = modifier.height(Dimens.margin16))
     }
 
 }
