4c4fd41eba8c8885da6398348597937554b192cd
SANDY <nnal0256@naver.com>
2025-01-13 05:16:03 +0900

    feat: 미션 바로가기 배너 구현

--- feature/exp/src/main/java/com/tenday/feature/exp/MyExpScreen.kt
+++ feature/exp/src/main/java/com/tenday/feature/exp/MyExpScreen.kt
@@ -1,10 +1,13 @@
 package com.tenday.feature.exp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tenday.designsystem.theme.Gray100
+import com.tenday.feature.exp.components.ExpMissionBanner
 import com.tenday.feature.exp.components.MyExpProfile
 
 @Composable
@@ -17,10 +20,15 @@ internal fun MyExpScreen(
     modifier: Modifier = Modifier
 ) {
     LazyColumn(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Gray100),
     ) {
         item {
             MyExpProfile("F2-Ⅱ", currentValue = 14000,)
+        }
+        item {
+            ExpMissionBanner("김민수")
         }
     }
 }
