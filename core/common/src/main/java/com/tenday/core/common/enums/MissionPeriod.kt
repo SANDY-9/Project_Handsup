b8c21c8c31856581c0de29fa91981100f502c8a9
SANDY <nnal0256@naver.com>
2025-01-15 01:53:20 +0900

    add: MissionPeriod에 desc, query 변수 추가
    기존 desc변수를 json 변환을 위한 query로 변경하고
    새롭게 이름을 저장하도록 기능 변경

--- core/common/src/main/java/com/tenday/core/common/enums/MissionPeriod.kt
+++ core/common/src/main/java/com/tenday/core/common/enums/MissionPeriod.kt
@@ -1,15 +1,18 @@
 package com.tenday.core.common.enums
 
-enum class MissionPeriod(val desc: String) {
-    MONTH("month"),
-    WEEK("week"),
-    DAY("day"),
-    NULL("");
+enum class MissionPeriod(
+    val query: String,
+    val desc: String,
+) {
+    MONTH(query = "month", desc = "월간 미션"),
+    WEEK(query = "week", desc = "주간 미션"),
+    DAY(query = "day", desc = "일간 미션"),
+    NULL(query = "", desc = "");
 
     companion object {
         fun getPeriod(period: String): MissionPeriod {
             return MissionPeriod.entries.firstOrNull {
-                it.desc == period
+                it.query == period
             } ?: NULL
         }
     }
