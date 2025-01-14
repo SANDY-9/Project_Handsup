1c9b224408eba8b4ff9d195ccaaf019e7e24f10d
SANDY <nnal0256@naver.com>
2025-01-15 00:39:01 +0900

    feat: 미션화면 상태 구현

--- feature/mission/src/main/java/com/tenday/feature/mission/MissionViewModel.kt
+++ feature/mission/src/main/java/com/tenday/feature/mission/MissionViewModel.kt
@@ -1,20 +1,22 @@
 package com.tenday.feature.mission
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tenday.core.domain.usecases.mission.GetJobMissionUseCase
 import com.tenday.core.domain.usecases.mission.GetLeaderMissionUseCase
 import com.tenday.core.domain.usecases.mission.GetPersonnelMissionUseCase
 import com.tenday.core.domain.usecases.mission.GetProjectMissionUseCase
+import com.tenday.feature.mission.model.HandsUpMission
 import com.tenday.feature.mission.model.MissionUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -28,39 +30,99 @@ internal class MissionViewModel @Inject constructor(
     private val _currentTab: MutableStateFlow<MissionMenu> = MutableStateFlow(MissionMenu.리더부여)
     val currentTab = _currentTab.asStateFlow()
 
+    private val _missionUiState: MutableStateFlow<MissionUiState> = MutableStateFlow(MissionUiState.Loading)
+    val missionUiState = _missionUiState.asStateFlow()
+
+    private val missionItem = mutableMapOf<MissionMenu, HandsUpMission>()
+    private fun setMissionItem(missionMenu: MissionMenu, handsUpMission: HandsUpMission) {
+        missionItem[missionMenu] = handsUpMission
+    }
+
     init {
-        getLeaderMissionUseCase().onStart {
-            Log.e("확인", "로딩 ", )
-        }.onEach {
-            Log.e("확인", "$it ", )
-        }.catch {
-            Log.e("확인", "$it ", )
-        }.launchIn(viewModelScope)
+        viewModelScope.launch {
+            currentTab.collectLatest {
+                when(it) {
+                    MissionMenu.리더부여 -> fetchLeaderMission()
+                    MissionMenu.직무미션 -> fetchJobMission()
+                    MissionMenu.전사프로젝트 -> fetchProjectMission()
+                    MissionMenu.인사평가 -> fetchPersonnelMission()
+                }
+            }
+        }
+    }
 
-        getJobMissionUseCase().onStart {
-            Log.e("확인", "로딩 ")
-        }.onEach {
-            Log.e("확인", "$it ")
-        }.catch {
-            Log.e("확인", "$it ")
-        }.launchIn(viewModelScope)
+    fun updateCurrentTabState(tab: MissionMenu) {
+        _currentTab.value = tab
+    }
 
-        getProjectMissionUseCase().onStart {
-            Log.e("확인", "로딩 ")
-        }.onEach {
-            Log.e("확인", "$it ")
-        }.catch {
-            Log.e("확인", "$it ")
-        }.launchIn(viewModelScope)
+    private fun updateMissionUiState(mission: HandsUpMission) {
+        _missionUiState.value = MissionUiState.Success(data = mission)
+    }
 
-        getPersonnelMissionUseCase().onStart {
-            Log.e("확인", "로딩 ")
-        }.onEach {
-            Log.e("확인", "$it ")
-        }.catch {
-            Log.e("확인", "$it ")
-        }.launchIn(viewModelScope)
+    private fun changeMissionState(missionMenu: MissionMenu): Boolean {
+        val mission = missionItem[missionMenu] ?: return false
+        updateMissionUiState(mission)
+        return true
+    }
 
+    private fun fetchLeaderMission() {
+        val changed = changeMissionState(MissionMenu.리더부여)
+        if(!changed) {
+            getLeaderMissionUseCase().onStart {
+                _missionUiState.value = MissionUiState.Loading
+            }.onEach { data ->
+                val leaderMission = HandsUpMission.Leader(data)
+                updateMissionUiState(leaderMission)
+                setMissionItem(MissionMenu.리더부여, leaderMission)
+            }.catch {
+                _missionUiState.value = MissionUiState.Fail
+            }.launchIn(viewModelScope)
+        }
+    }
+
+    private fun fetchJobMission() {
+        val changed = changeMissionState(MissionMenu.직무미션)
+        if(!changed) {
+            getJobMissionUseCase().onStart {
+                _missionUiState.value = MissionUiState.Loading
+            }.onEach { data ->
+                val jobMission = HandsUpMission.Job(data)
+                updateMissionUiState(jobMission)
+                setMissionItem(MissionMenu.직무미션, jobMission)
+            }.catch {
+                _missionUiState.value = MissionUiState.Fail
+            }.launchIn(viewModelScope)
+        }
+    }
+
+    private fun fetchProjectMission() {
+        val changed = changeMissionState(MissionMenu.전사프로젝트)
+        if(!changed) {
+            getProjectMissionUseCase().onStart {
+                _missionUiState.value = MissionUiState.Loading
+            }.onEach { data ->
+                val projectMission = HandsUpMission.Project(data)
+                updateMissionUiState(projectMission)
+                setMissionItem(MissionMenu.전사프로젝트, projectMission)
+            }.catch {
+                _missionUiState.value = MissionUiState.Fail
+            }.launchIn(viewModelScope)
+        }
+    }
+
+    private fun fetchPersonnelMission() {
+        val changed = changeMissionState(MissionMenu.인사평가)
+        if(!changed) {
+            getPersonnelMissionUseCase().onStart {
+                _missionUiState.value = MissionUiState.Loading
+            }.onEach { data ->
+                val personnelMission = HandsUpMission.Personnel(data)
+                updateMissionUiState(personnelMission)
+                setMissionItem(MissionMenu.인사평가, personnelMission)
+            }.catch {
+                _missionUiState.value = MissionUiState.Fail
+            }.launchIn(viewModelScope)
+        }
     }
 
 }
--- feature/mission/src/main/java/com/tenday/feature/mission/model/HandsUpMission.kt
+++ feature/mission/src/main/java/com/tenday/feature/mission/model/HandsUpMission.kt
@@ -2,23 +2,24 @@ package com.tenday.feature.mission.model
 
 import com.tenday.core.model.JobMission
 import com.tenday.core.model.LeaderMission
+import com.tenday.core.model.PersonnelMission
 import com.tenday.core.model.ProjectMission
 
 internal sealed interface HandsUpMission {
     data class Leader(
         val data: LeaderMission,
         val name: String = "리더부여",
-    )
+    ): HandsUpMission
     data class Job(
         val data: JobMission,
         val name: String = "직무미션",
-    )
+    ): HandsUpMission
     data class Project(
-        val data: ProjectMission,
+        val data: List<ProjectMission>,
         val name: String = "전사프로젝트",
-    )
+    ): HandsUpMission
     data class Personnel(
-        val data: Personnel,
+        val data: List<PersonnelMission>,
         val name: String = "인사평가",
-    )
+    ): HandsUpMission
 }
