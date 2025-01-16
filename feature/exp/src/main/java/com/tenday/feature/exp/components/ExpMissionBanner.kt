3153eb9665b0f87ccdbe44f8fbe0ff0b9f2a8f37
SANDY <nnal0256@naver.com>
2025-01-16 12:55:55 +0900

    fix: 미션이동배너 디자인 변경 사항 적용

--- feature/exp/src/main/java/com/tenday/feature/exp/components/ExpMissionBanner.kt
+++ feature/exp/src/main/java/com/tenday/feature/exp/components/ExpMissionBanner.kt
@@ -7,20 +7,24 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tenday.designsystem.dimens.Dimens
+import com.tenday.designsystem.icons.ArrowBack
 import com.tenday.designsystem.icons.Dodoong
-import com.tenday.designsystem.theme.HandsUpOrange
 import com.tenday.designsystem.theme.HandsUpTypography
+import com.tenday.designsystem.theme.LinearGradientOrangeBold
 import com.tenday.designsystem.theme.White
 import com.tenday.feature.exp.R
 
@@ -33,7 +37,7 @@ internal fun ExpMissionBanner(
         modifier = modifier
             .fillMaxWidth()
             .background(
-                color = HandsUpOrange,
+                brush = LinearGradientOrangeBold,
                 shape = RoundedCornerShape(Dimens.cornerShape12)
             )
             .padding(
@@ -54,6 +58,13 @@ internal fun ExpMissionBanner(
             modifier = modifier.size(48.dp),
             imageVector = Icons.Dodoong,
             contentDescription = null,
+        )
+        Spacer(modifier = modifier.width(20.dp))
+        Icon(
+            modifier = modifier.size(24.dp).rotate(180f),
+            imageVector = Icons.ArrowBack,
+            contentDescription = null,
+            tint = White,
         )
     }
 }
