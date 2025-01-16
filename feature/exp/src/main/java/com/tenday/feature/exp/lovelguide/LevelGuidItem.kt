0710b479fb84f8c716a5a4ff5159892ae7e5051a
SANDY <nnal0256@naver.com>
2025-01-17 01:03:52 +0900

    fix: 직군별 선택 레벨 컬러 다르게 설정

--- feature/exp/src/main/java/com/tenday/feature/exp/lovelguide/LevelGuidItem.kt
+++ feature/exp/src/main/java/com/tenday/feature/exp/lovelguide/LevelGuidItem.kt
@@ -11,19 +11,28 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tenday.core.common.enums.HandsUpLevel
+import com.tenday.core.common.enums.JobFamily
 import com.tenday.core.common.extentions.toData
 import com.tenday.designsystem.dimens.Dimens
 import com.tenday.designsystem.extentions.svgImageLoader
 import com.tenday.designsystem.theme.Gray900
+import com.tenday.designsystem.theme.HandsUpBlue
+import com.tenday.designsystem.theme.HandsUpBlueSub
+import com.tenday.designsystem.theme.HandsUpGreen
+import com.tenday.designsystem.theme.HandsUpGreenSub
 import com.tenday.designsystem.theme.HandsUpOrange
 import com.tenday.designsystem.theme.HandsUpOrangeSub
+import com.tenday.designsystem.theme.HandsUpPurple
+import com.tenday.designsystem.theme.HandsUpPurpleSub
 import com.tenday.designsystem.theme.HandsUpTypography
 import com.tenday.designsystem.theme.White
 
@@ -33,11 +42,13 @@ internal fun LevelGuidItem(
     isMatchedLevel: Boolean,
     modifier: Modifier = Modifier,
 ) {
+    val majorColor = remember { level.getColorMajor() }
+    val subColor = remember { level.getColorSub() }
     Row(
         modifier = modifier
             .fillMaxWidth()
             .background(
-                color = if(isMatchedLevel) HandsUpOrangeSub else White,
+                color = if(isMatchedLevel) subColor else White,
             ).padding(
                 start = Dimens.margin16,
                 end = Dimens.margin12,
@@ -61,7 +72,7 @@ internal fun LevelGuidItem(
                 Spacer(modifier = modifier.width(Dimens.margin4))
                 Text(
                     modifier = modifier.background(
-                        color = if(isMatchedLevel) HandsUpOrange else White,
+                        color = if(isMatchedLevel) majorColor else White,
                         shape = RoundedCornerShape(Dimens.cornerShape4),
                     ).padding(
                         horizontal = Dimens.margin6,
@@ -81,10 +92,30 @@ internal fun LevelGuidItem(
             text = level.exp.toData() + "do",
             style = HandsUpTypography.body2.copy(
                 fontWeight = if(isMatchedLevel) FontWeight.Bold else FontWeight.SemiBold,
-                color = if(isMatchedLevel) HandsUpOrange else Gray900,
+                color = if(isMatchedLevel) majorColor else Gray900,
             ),
             textAlign = TextAlign.End,
         )
+    }
+}
+
+private fun HandsUpLevel.getColorMajor(): Color {
+    return when(group) {
+        JobFamily.F -> HandsUpOrange
+        JobFamily.B -> HandsUpBlue
+        JobFamily.G -> HandsUpGreen
+        JobFamily.T -> HandsUpPurple
+        else -> HandsUpOrange
+    }
+}
+
+private fun HandsUpLevel.getColorSub(): Color {
+    return when(group) {
+        JobFamily.F -> HandsUpOrangeSub
+        JobFamily.B -> HandsUpBlueSub
+        JobFamily.G -> HandsUpGreenSub
+        JobFamily.T -> HandsUpPurpleSub
+        else -> HandsUpOrangeSub
     }
 }
 
