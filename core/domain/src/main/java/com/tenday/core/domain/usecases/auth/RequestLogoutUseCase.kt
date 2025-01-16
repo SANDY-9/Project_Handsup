b8b31c88ee07088971234223a85e3db030b0d0d7
SANDY <nnal0256@naver.com>
2025-01-16 17:36:34 +0900

    add: RequestLogoutUseCase에 서버 로그아웃 추가

--- core/domain/src/main/java/com/tenday/core/domain/usecases/auth/RequestLogoutUseCase.kt
+++ core/domain/src/main/java/com/tenday/core/domain/usecases/auth/RequestLogoutUseCase.kt
@@ -1,12 +1,16 @@
 package com.tenday.core.domain.usecases.auth
 
 import com.tenday.core.domain.repository.AuthPrefsRepository
+import com.tenday.core.domain.repository.LoginRepository
 import javax.inject.Inject
 
 class RequestLogoutUseCase @Inject constructor(
+    private val loginRepository: LoginRepository,
     private val authPrefsRepository: AuthPrefsRepository,
 ) {
     suspend operator fun invoke() {
+        val token = authPrefsRepository.getAccessToken() ?: ""
+        loginRepository.requestLogout(token)
         authPrefsRepository.deleteAccessToken()
     }
 }
