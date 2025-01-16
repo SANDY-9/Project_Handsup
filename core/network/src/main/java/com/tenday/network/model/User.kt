f5be210975897138a080f0f0af9eddb44bc1f2a7
SANDY <nnal0256@naver.com>
2025-01-16 17:21:37 +0900

    fix: 뱃지코드 파싱 수정

--- core/network/src/main/java/com/tenday/network/model/User.kt
+++ core/network/src/main/java/com/tenday/network/model/User.kt
@@ -1,5 +1,6 @@
 package com.tenday.network.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -11,11 +12,19 @@ data class UserResponse(
     val jobPosition: Int,
     val jobGroup: Int,
     val jobLevel: String,
-    val possibleBadgeCodeList: List<String>,
+    val possibleBadgeCodeList: List<PossibleBadgeCode>,
     val profileBadgeCode: String? = null,
     val profileImageCode: String,
     val totalExpLastYear: Int,
     val username: String
+)
+
+@Serializable
+data class PossibleBadgeCode(
+    @SerialName("badgeCode")
+    val badgeCode: String,
+    @SerialName("createdAt")
+    val createdAt: String
 )
 
 @Serializable
